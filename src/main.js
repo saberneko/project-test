b959df8be1f3342da2b0ee7af87a408dfef74e37
ChanRay <richardo2016@gmail.com>
2016-08-09 12:39:53 +0800

    增加 vue-resource, 使用 vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
+import Router from 'vue-router'
+import Resource from 'vue-resource'
+Vue.use(Router)
+Vue.use(Resource)
+
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// routing
+let router = new Router({
+  hashbang: false, // disable hashbang though in hash mode
+  // history: true // enable history mode
 })
+
+let emptyBase = Vue.extend({
+  name: 'Base',
+  template:
+    '<div class="current-area">' +
+      '<router-view></router-view>' + // <- 嵌套的外链
+    '</div>'
+})
+
+// import * as Widgets from './views/widgets'
+
+router.map({
+  '/': {
+    component: emptyBase
+  }
+})
+
+router.beforeEach(function () {
+  // 动态判定 router
+  window.scrollTo(0, 0)
+})
+
+router.start(App, '#app')
