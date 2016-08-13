4f87cacedcfd8f7db2bbe97f24576392991bc481
ChanRay <richardo2016@gmail.com>
2016-08-13 15:32:00 +0800

    adjust eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,29 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    'indent': ['error', 'tab'],
+    'indent': ['error', 'tab', { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 }, "SwitchCase": 1 }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    //  allows the opening and closing braces for a block to be on the same line
+    // 'allowSingleLine': 0,
+    // http://eslint.org/docs/rules/brace-style
+    'brace-style': [
+      'error',
+      // 'stroustrup', 'allman'
+      '1tbs',
+      { "allowSingleLine": true }
+    ],
+    "camelcase": [0, {"properties": "never"}],
+    // close no-unused-vars when debug
+    "no-unused-vars": [process.env.NODE_ENV === 'production' ? 2 : 0],
+    "one-var": [0],
+    // forbid no multiple spaces except property of object, variable-declaration and import-declaration
+    "no-multi-spaces": ["error", {
+      exceptions: {
+        "Property": true,
+        "VariableDeclarator": true,
+        "ImportDeclaration": true
+      },
+    }]
+  },
 }
