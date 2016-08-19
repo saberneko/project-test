1ccaf1f337e4a7b59a1d1cac117579f47baaf709
saberneko <471632743@qq.com>
2016-08-19 23:46:35 +0800

    toplists+hotmatters-lists+mui-scroll

--- src/view/focus.vue
+++ src/view/focus.vue
@@ -41,10 +41,23 @@
 			</div>
 		</div>
 		<ul class="mui-table-view mui-grid-view mui-grid-9" style="background-color: #FFFFFF;">
-			<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4"></li>
+			<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4" v-for="($index,topItem) in lists">
+				<a>
+					<img class="img-icon" :src="'/static/img/img'+$index+'.ico'">
+					<div class="font-color img-icon-title">{{topItem.GP_name}}</div>
+				</a>
+			</li>
 		</ul>
-		<ul class="mui-table-view " id="Hot5ProjectList">
-
+		<ul class="mui-table-view">
+			<li class="mui-table-view-cell mui-media" v-for="hotItem in hotLists">
+				<a >
+					<img class="mui-media-object mui-pull-left hot-list-img" src="../assets/images/top.net.ico">
+					<div class="mui-media-body mui-ellipsis hot-list-fontsize" style="font-size:15px">{{hotItem.GP_name}}
+					<p class="mui-ellipsis hot-list-p" >关注度:{{hotItem.GP_hot}}</p>
+					<p class="mui-ellipsis hot-list-p" >截止时间:{{hotItem.GP_Time}}</p>
+					</div>
+				</a>
+			</li>
 		</ul>
 	</div>
 </template>
@@ -54,22 +67,31 @@ import mui from '../lib/mui.min.js'
 import R from '../common/request'
 import { GP } from '../common/index'
 
+function requireAll (r) { r.keys().forEach(r) }
+requireAll(require.context('../assets/images/', true, /\.jpe?g|png|ico$/))
+
 export default {
 
 	name: 'focus',
 
 	ready () {
-		// let self = this
+		let self = this
 
 		R.post('/Service/GetBigMatterList.ashx').then(function (data) {
-			// this.$data.list = data
-			console.log(data)
+			self.lists = data
+		})
+
+		R.post('/Service/GetHot5ProjectList.ashx',{
+			'_flag': 're'
+		}).then(function (data) {
+			self.hotLists = data
 		})
 	},
 
 	data () {
 		return {
-			// list: ''
+			lists: null,
+			hotLists: null
 		}
 	}
 
@@ -77,4 +99,17 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.hot-list-img{
+	width:30px;
+	height: 32px;
+	margin-top: 10px;
+	margin-right: 15px;
+}
+.hot-list-fontsize{
+	font-size:15px;
+}
+.hot-list-p{
+	margin-right: 30px;
+	font-size:12px;
+}
 </style>
