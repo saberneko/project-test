f1153f59099b84a43c1f0970477c1dab7439f0d0
saberneko <471632743@qq.com>
2016-08-19 23:45:01 +0800

    计算属性

--- src/view/class-style.vue
+++ src/view/class-style.vue
@@ -4,11 +4,21 @@
 	<div v-bind:class="[classA, isB ? classB : '']">为class绑定 -数组语法</div>
 	<div v-bind:class="classObject">为class绑定-对象语法</div>
 	<div v-bind:style="styleObject">绑定内联样式</div>
+	a={{a}},b={{b}}
+	<div>计算属性fullName: {{fullName}}</div>
+	<div>firstName: {{firstName}} / lastName: {{lastName}}</div>
+	<input type="text" v-model="changeName">
+	<p>changeName:{{changeName}}</p>
+	<button @click="setProperty">赋值fullName</button>
 </template>
 
 <script>
 export default {
 	name: 'main',
+
+	ready () {
+		this.a = 3
+	},
 
 	data () {
 		return {
@@ -21,6 +31,33 @@ export default {
 			styleObject: {
 				color: 'blue',
 				fontSize: '13px'
+			},
+			a: 1,
+			firstName: 'Foo',
+			lastName: 'Bar',
+			changeName: null
+		}
+	},
+
+	methods: {
+		setProperty: function () {
+			this.fullName = this.changeName
+		}
+	},
+
+	computed: {
+		b: function () {
+			return this.a + 1
+		},
+		fullName: {
+			// 计算属性默认getter
+			get: function () {
+				return this.firstName + ' ' + this.lastName// 依赖firstName和lastName
+			},
+			set: function (newValue) {
+				let names = newValue.split(' ')
+				this.firstName = names[0]
+				this.lastName = names[names.length - 1]
 			}
 		}
 	}
