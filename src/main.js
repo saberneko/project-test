f260032a3d76e4af1624edfe5194c0dd48448910
ChanRay <richardo2016@gmail.com>
2016-08-21 13:01:26 +0800

    SPA->MPA

--- src/main.js
+++ src/main.js
@@ -8,26 +8,22 @@ import App from './App'
 
 // routing
 let router = new Router({
-	hashbang: false// disable hashbang though in hash mode
-	// history: true // enable history mode
+	hashbang: false,// disable hashbang though in hash mode
+	history: true, // enable history mode
+	mode: 'html5'
 })
 
-let emptyBase = Vue.extend({
+let emptyRouteNode = Vue.extend({
 	name: 'Base',
 	template:
-		'<div class="current-area">Home Page' +
+		'<div class="current-area">' +
 			'<router-view></router-view>' + // <- 嵌套的外链
 		'</div>'
 })
 
-let Bar = Vue.extend({
-	name: 'Bar',
-	template: '<p>This is from bar</p>'
-})
-
 router.map({
 	'/': {
-		component: emptyBase
+		component: emptyRouteNode
 	},
 	'login': {
 		component: require('./view/login')
