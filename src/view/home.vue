9a3069ca1bf23c5d7337810710444556554339f4
saberneko <13588251053@163.com>
2016-08-25 13:59:53 +0800

    complete bottombar switching

---
+++ src/view/home.vue
@@ -0,0 +1,67 @@
+<template>
+	<div id="subdiv_index">
+		<div id="segmentedControl" class="mui-segmented-control" style="top:40px;z-index:999">
+			<a class="mui-control-item mui-active" v-touch:tap="setDisplay('focus')" @click="setDisplay('focus')">
+						我要关注
+			</a>
+			<a class="mui-control-item" v-touch:tap="setDisplay('expert')" @click="setDisplay('expert')">
+						谁是大神
+			</a>
+			<a class="mui-control-item" v-touch:tap="setDisplay('statics')" @click="setDisplay('statics')">
+						统计预测
+			</a>
+		</div>
+		<div id="pullrefresh" class="mui-content mui-scroll-wrapper" style="background-color: #efeff4;top: 90px;">
+			<div class="mui-scroll">
+				<focus v-if="mode === 'focus'"></focus>
+				<expert v-if="mode === 'expert'"></expert>
+				<statics v-if="mode === 'statics'"></statics>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import mui from '../lib/mui.js'
+import focus from './focus.vue'
+import expert from './expert.vue'
+import statics from './statics.vue'
+
+export default {
+
+	name: 'home',
+
+	ready () {
+		mui('.mui-scroll-wrapper').scroll({
+			deceleration: 0.0005
+		})
+
+		var slider = mui('#slider')
+		slider.slider({
+			interval: 5000
+		})
+	},
+
+	data () {
+		return {
+			mode: 'focus'
+		}
+	},
+
+	components: {
+		focus,
+		expert,
+		statics
+	},
+
+	methods: {
+		setDisplay: function (info) {
+			this.mode = info
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+@import '../assets/css/adding.css'
+</style>
---
+++ src/view/personal.vue
@@ -0,0 +1,80 @@
+<template>
+	<div id="subdiv_setting" class="mui-content">
+		<div id="tabbar-with-setting">
+			<ul class="mui-table-view mui-table-view-chevron" style="margin:10px 0px 15px 0px;">
+				<li class="mui-table-view-cell mui-media mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/MyProfile.html">
+						<img id="userimg" class="mui-media-object mui-pull-left" src="../assets/images/user-photo.png">
+						<div class="mui-media-body">
+							<label id="TLM_name"></label>
+							<p class='mui-ellipsis' id="TLM_gh"></p>
+						</div>
+					</a>
+				</li>
+			</ul>
+			<ul class="mui-table-view" style="margin-top: 15px;">
+				<li id="MyProjectListA" class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="MyProjectList" onclick="">
+						点滴记录
+					</a>
+				</li>
+				<!--add yours-->
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/SeekScore.html" onclick="NewOpenWindow(this.id);">
+						成绩查询
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/MyRank.html" onclick="NewOpenWindow(this.id);">
+						我的排名
+					</a>
+				</li>
+			</ul>
+
+			<ul class="mui-table-view" style="margin-top: 15px;">
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/MyApplicationList.html" onclick="">
+						系统消息
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/MyApplicationList.html" onclick="">
+						意见反馈
+					</a>
+				</li>
+			</ul>
+
+			<ul class="mui-table-view" style="margin-top: 15px;">
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a class="mui-navigate-right" id="My/aboutus.html" onclick="NewOpenWindow(this.id);">
+						关于我们
+					</a>
+				</li>
+			</ul>
+
+			<ul class="mui-table-view" style="margin-top: 25px;">
+				<li class="mui-table-view-cell mui-table-view-cellExt">
+					<a id="exit" style="text-align: center;color: #FF3B30;">
+						退出
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+	name: 'personal',
+
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+</style>
